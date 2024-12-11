3b2d9d320cbc66302942177d36cfef2db1fe0ef8
Alif-N <alifnasrullah05@gmail.com>
2024-12-11 16:45:43 +0700

    Upload Code

--- function.php
+++ function.php
@@ -23,11 +23,33 @@ if(isset($_POST['login']))
         echo '
         <script>
             alert("Username atau Password salah")
-            window.location.href=login.php
+            window.location.href="login.php"
         </script>
         ';
     }
 }
 
+if(isset($_POST['tambahBarang']))
+{
+    $namaProduk = $_POST['namaProduk'];
+    $deskripsi = $_POST['deskripsi'];
+    $stock = $_POST['stock'];
+    $harga = $_POST['harga'];
+
+    $insert = mysqli_query($conn, "insert into produk (namaProduk, deskripsi, harga, stock) values ('$namaProduk', '$deskripsi', '$harga', '$stock')");
+
+    if($insert)
+    {
+        header('location:stock.php');
+    } else
+    {
+        echo '
+        <script>
+            alert("Gagal Menambah Barag Baru")
+            window.location.href="stock.php"
+        </script>
+        ';
+    }
+}
 
 ?>
